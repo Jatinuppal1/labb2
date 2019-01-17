568135d068ad5115cc6971642d9cc287b030ae4b
Jatin Uppal <jatinuppal89@gmail.com>
2019-01-17 10:38:47 +0530

    make add and sub

--- file1.c
+++ file1.c
@@ -1,6 +1,19 @@
-#include<iostream>
+#include<stdio.h>
 int main()
 {
-    cout<<"hello world";
+    int a,b,c;
+    printf("what do you want to perform\n1.add\n2.sub\n");
+    scanf("%d",&c);
+    printf("Enter numbers\n");
+    scanf("%d%d",&a,&b);
+    shift(c)
+    {
+	    case 1:
+		    c=a+b;
+break:
+	    case 2:
+		    c=a-b;
+    }
+    
     return 0;
 }
