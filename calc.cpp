4590bab7dc6ba872c15e31e08b1594f591475359
Jatin Uppal <jatinuppal89@gmail.com>
2019-02-14 05:22:58 +0530

    function definition provided

--- calc.cpp
+++ calc.cpp
@@ -3,11 +3,32 @@ using namespace std;
 class Calculator
 {
    public:
+	   Calculator()
+	   {
+		   cout<<"Enter nos.\n";
+		   cin>>a>>b;
+	   }
 	   float a,b;
 	   float add();
 	   float sub();
 	   float mult();
 	   float div();
+};
+float Calculator::add()
+{
+	cout<<"\nAddition of nos. is "<<a+b<<endl;
+}
+float Calculator::sub()
+{
+	cout<<"\nSubtraction of nos. is "<<a-b<<endl;
+}
+float Calculator::mult()
+{
+	cout<<"\nMUltiplication of nos. is "<<a*b<<endl;
+}
+float Calculator::div()
+{
+	cout<<"\nDivision of nos. is "<<a/b<<endl;
 }
 int main()
 {
