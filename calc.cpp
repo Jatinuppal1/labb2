dd60988ac633ae93f6cee447a0fe8dc342b3d7ac
Jatin Uppal <jatinuppal89@gmail.com>
2019-02-14 05:38:41 +0530

    calculator program completed.

--- calc.cpp
+++ calc.cpp
@@ -32,6 +32,30 @@ float Calculator::div()
 }
 int main()
 {
-	Calculator c1;
+	int a;
+	cout<<"_____Calculator_____"<<endl;
+	cout<<"1. Addition\n2. Subtaction\n3. Multiplication\n4. Division\n";
+	do
+	{
+		cout<<"Enter your choice\n";
+		cin>>a;
+		Calculator c1;
+		switch(a)
+		{
+			case 1: c1.add();
+				break;
+			case 2: c1.sub();
+				break;
+			case 3: c1.mult();
+				break;
+			case 4: c1.div();
+				break;
+			default:
+				cout<<"\nTry again";
+				a=5;
+		}
+		cout<<"\nFor exit press 0\n";
+		cin>>a;
+	}while(a=!0)
 	return 0;
 }
